ed3bc3a187a43042b10ccb80e3ad395cf6bf334c
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2020-11-14 00:01:47 +0500

    Create expense tracker using react typescript with PWA functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//import components
+
+import Header from './components/Header';
+import CurrentBalance from './components/CurrentBalance';
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionHistory } from './components/TransactionHistory';
+import { AddTransaction } from './components/AddTransaction';
+
+//import global context
+import { GlobalProvider } from './components/context/GlobalState';
+import {configNotification} from './firebase';
+
+//1 Expense tracker by Muhammad Muneeb Waseem is one part, all other 4 components
+//lies in one container, thats why I created another div inside main div with 
+//className= "container"
+
+    // {/* <div className="container">
+    // <CurrentBalance />
+    // </div> */}
+
+
 function App() {
+  configNotification()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Header />
+
+      <div className="container">
+        <CurrentBalance />
+        <IncomeExpense />
+        <TransactionHistory />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
